c7773c8b0f1e3ba513d13825c43749ec2a7d1170
jenbitcoin <jen@bitcoin.com>
2023-01-06 11:53:34 +0800

    Update build_secp256k1.sh
    https://github.com/RandyMcMillan/BitcoinKit/commit/ae3f34d5026eae021b0267a583b600ced0307fb6

--- setup/build_secp256k1.sh
+++ setup/build_secp256k1.sh
@@ -1,36 +1,73 @@
 #!/bin/sh
 set -ex
 
-SCRIPT_DIR=`dirname "$0"`
-
+SCRIPT_DIR=`pwd`
+export SCRIPT_DIR
+echo $SCRIPT_DIR
+ls $SCRIPT_DIR
+# exit
 TDIR=`mktemp -d`
 trap "{ cd - ; rm -rf $TDIR; exit 255; }" SIGINT
 
 cd $TDIR
 
-git clone https://github.com/bitcoin-core/secp256k1.git src
+git clone --depth 1 https://github.com/bitcoin-core/secp256k1.git src
 
-CURRENTPATH=`pwd`
+CURRENTPATH=$PWD
+export CURRENTPATH
+ls $CURRENTPATH
 
-TARGETDIR_IPHONEOS="$CURRENTPATH/.build/iphoneos"
+IPHONE_SIMULATOR=iphonesimulator
+IPHONEOS=iphoneos
+mkdir -p $TDIR/$IPHONE_SIMULATOR
+mkdir -p $TDIR/$IPHONEOS
+export TARGETDIR_SIMULATOR
+
+TARGETDIR_IPHONEOS="$SCRIPT_DIR/.build/$IPHONEOS"
 mkdir -p "$TARGETDIR_IPHONEOS"
+export TARGETDIR_IPHONEOS
 
-TARGETDIR_SIMULATOR="$CURRENTPATH/.build/iphonesimulator"
+TARGETDIR_SIMULATOR="$SCRIPT_DIR/.build/$IPHONE_SIMULATOR"
 mkdir -p "$TARGETDIR_SIMULATOR"
+export TARGETDIR_SIMULATOR
 
+ls $SCRIPT_DIR
+ls $CURRENTPATH
+ls $TDIR
+
+HOST_ARCH=$(uname -m)
+
+xcrun -sdk iphoneos --show-sdk-path
+alias autoreconf=/opt/homebrew/bin/autoreconf
 (cd src && ./autogen.sh)
-(cd src && ./configure --host=x86_64-apple-darwin CC=`xcrun -find clang` CFLAGS="-O3 -arch i386 -arch x86_64 -isysroot `xcrun -sdk iphonesimulator --show-sdk-path` -fembed-bitcode-marker -mios-simulator-version-min=8.0" CXX=`xcrun -find clang++` CXXFLAGS="-O3 -arch i386 -arch x86_64 -isysroot `xcrun -sdk iphonesimulator --show-sdk-path` -fembed-bitcode-marker -mios-simulator-version-min=8.0" --prefix="$TARGETDIR_IPHONEOS" && make install)
-(cd src && make distclean > /dev/null)
-(cd src && ./autogen.sh)
-(cd src && ./configure --host=arm-apple-darwin CC=`xcrun -find clang` CFLAGS="-O3 -arch armv7 -arch armv7s -arch arm64 -isysroot `xcrun -sdk iphoneos --show-sdk-path` -fembed-bitcode -mios-version-min=8.0" CXX=`xcrun -find clang++` CXXFLAGS="-O3 -arch armv7 -arch armv7s -arch arm64 -isysroot `xcrun -sdk iphoneos --show-sdk-path` -fembed-bitcode -mios-version-min=8.0" --prefix="$TARGETDIR_SIMULATOR" && make install)
+
+(cd src && ./configure --host=arm-apple-darwin \
+    CC=`xcrun -find clang` \
+    CFLAGS="-O3 -isysroot \
+    `xcrun -sdk $IPHONE_SIMULATOR --show-sdk-path` -fembed-bitcode-marker -mios-simulator-version-min=8.0" \
+    CXX=`xcrun -find clang++` \
+    CXXFLAGS="-O3 -isysroot \
+    `xcrun -sdk $IPHONE_SIMULATOR--show-sdk-path` -fembed-bitcode-marker -mios-simulator-version-min=8.0" --prefix="$TARGETDIR_SIMULATOR" \
+    && make install)
+
+mkdir -p "$SCRIPT_DIR/Libraries/$IPHONE_SIMULATOR/secp256k1/lib"
+xcrun lipo -create "$TARGETDIR_SIMULATOR/lib/libsecp256k1.a" \
+                   -o "$SCRIPT_DIR/Libraries/$IPHONE_SIMULATOR/secp256k1/lib/libsecp256k1.a"
+cp -rf $TDIR/src/include "$SCRIPT_DIR/Libraries/$IPHONE_SIMULATOR/secp256k1"
+exit
+(cd src && ./configure --host=arm-apple-darwin \
+     CC=`xcrun -find clang` CFLAGS="-O3 -isysroot \
+     `xcrun -sdk $IPHONEOS --show-sdk-path` -fembed-bitcode -mios-version-min=8.0" \
+     CXX=`xcrun -find clang++` \
+     CXXFLAGS="-O3 -isysroot \
+     `xcrun -sdk $IPHONEOS        --show-sdk-path` -fembed-bitcode -mios-version-min=8.0" --prefix="$TARGETDIR_IPHONEOS" \
+     && make install)
 
 cd -
+mkdir -p "$SCRIPT_DIR/Libraries/$IPHONE_SIMULATOR/secp256k1/lib"
+xcrun lipo -create "$TARGETDIR_SIMULATOR/lib/libsecp256k1.a" \
+                   -o "$SCRIPT_DIR/Libraries/$IPHONE_SIMULATOR/secp256k1/lib/libsecp256k1.a"
 
-mkdir -p "$SCRIPT_DIR/../Libraries/secp256k1/lib"
-xcrun lipo -create "$TARGETDIR_IPHONEOS/lib/libsecp256k1.a" \
-                   "$TARGETDIR_SIMULATOR/lib/libsecp256k1.a" \
-                   -o "$SCRIPT_DIR/../Libraries/secp256k1/lib/libsecp256k1.a"
-cp -rf $TDIR/src/include "$SCRIPT_DIR/../Libraries/secp256k1"
 
 rm -rf $TDIR
 
